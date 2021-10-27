3694afcff493bca1acd113586b41d4716f4e301a
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-27 13:05:57 -0700

    For submit events on journal entry and verify new entries are saved to local stoagea long with the rest of the data model on page reload

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+var previousSumbitsStore = localStorage.getItem('javascript-local-storage');
+
+function storeData(event) {
+  var previousSubmitsJSON = JSON.stringify(data.entries);
+  localStorage.setItem('javascript-local-storage', previousSubmitsJSON);
+}
+
+if (previousSumbitsStore !== null) {
+  data.entries = JSON.parse(previousSumbitsStore);
+}
+
+window.addEventListener('beforeunload', storeData);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 /* global data */
 /* exported data */
-// var $img = document.querySelector('img');
-// // var $inputTitle = document.querySelector('input[type="text"]');
-// var $inputUrl = document.querySelector('input[type="url"]');
-// var $journalForm = document.querySelector('#journal-form');
-// function updatePhoto() {
-//   $img.src = $journalForm.elements.url.value;
-// }
-// // function submission() {
-// //   var inputValues = {
-// //     title: $journalForm.elements.title.value,
-// //     photo: $journalForm.elements.url.value
-// //   };
+var $img = document.querySelector('img');
+// var $inputTitle = document.querySelector('input[type="text"]');
+var $inputUrl = document.querySelector('input[type="url"]');
+// var $notes = document.querySelector('textarea');
+var $journalForm = document.querySelector('#journal-form');
 
-// // }
+function updatePhoto(event) {
+  $img.src = $journalForm.elements.url.value;
+}
+function submission(event) {
+  event.preventDefault();
+  var inputValues = {
+    title: $journalForm.elements.title.value,
+    photo: $journalForm.elements.url.value,
+    notes: $journalForm.elements.notes.value,
+    nextEntryId: data.nextEntryId++
+  };
+  data.entries.push(inputValues);
+  $img.src = 'images/placeholder-image-square.jpg';
+  $journalForm.reset();
+}
 
-// $inputUrl.addEventListener('input', updatePhoto);
-// // $journalForm.addEventListener('submit', submission);
+$inputUrl.addEventListener('input', updatePhoto);
+$journalForm.addEventListener('submit', submission);
