e354916fed8b8192a327b441e19e79f00ede21fd
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-26 20:38:18 -0700

    In progress

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 /* global data */
 /* exported data */
-var $img = document.querySelector('img');
-var $inputUrl = document.querySelector('input[type="url"]');
-var $journalForm = document.querySelector('#journal-form');
-function updatePhoto() {
-  $img.src = $journalForm.elements.url.value;
-}
+// var $img = document.querySelector('img');
+// // var $inputTitle = document.querySelector('input[type="text"]');
+// var $inputUrl = document.querySelector('input[type="url"]');
+// var $journalForm = document.querySelector('#journal-form');
+// function updatePhoto() {
+//   $img.src = $journalForm.elements.url.value;
+// }
+// // function submission() {
+// //   var inputValues = {
+// //     title: $journalForm.elements.title.value,
+// //     photo: $journalForm.elements.url.value
+// //   };
 
-$inputUrl.addEventListener('input', updatePhoto);
+// // }
+
+// $inputUrl.addEventListener('input', updatePhoto);
+// // $journalForm.addEventListener('submit', submission);
