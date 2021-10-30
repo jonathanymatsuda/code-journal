ccc736bb85693fc69d50784ff5e731d33d66b14f
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-30 00:33:50 -0700

    DOM Creation and styling changed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var $img = document.querySelector('img');
 var $inputUrl = document.querySelector('input[type="url"]');
 var $journalForm = document.querySelector('#journal-form');
+// var $ul = document.querySelector('#entries-list');
 
 function updatePhoto(event) {
   $img.src = $journalForm.elements.url.value;
@@ -23,11 +24,35 @@ function submission(event) {
 $inputUrl.addEventListener('input', updatePhoto);
 $journalForm.addEventListener('submit', submission);
 
-// function createEntries(entries) {
-//   var divDataView = document.createElement('div');
-//   divDataView.setAttribute('class', 'container');
-//   divDataView.setAttribute('data-view', 'entries');
+// function renderEntries(entries) {
+//   var $li = document.createElement('li');
+//   $ul.appendChild($li);
 
-//   var unorderedList = document.createElement('ul');
-//   divDataView.view.appendChild(unorderedList);
+//   var $divRow = document.createElement('div');
+//   $divRow.setAttribute('class', 'row');
+//   $li.appendChild($divRow);
+
+//   var $divColHalfOne = document.createElement('div');
+//   $divColHalfOne.setAttribute('class', 'column-half');
+//   $divRow.appendChild($divColHalfOne);
+
+//   var $img = document.createElement('img');
+//   $img.setAttribute('src', entries.photo);
+//   $img.setAttribute('alt', 'journal-image');
+//   $img.setAttribute('class', 'input-box-width border-radius');
+//   $divColHalfOne.appendChild($img);
+
+//   var $divColHalfTwo = document.createElement('div');
+//   $divColHalfTwo.setAttribute('class', 'column-half');
+//   $divRow.appendChild($divColHalfTwo);
+
+//   var $h2 = document.createElement('h2');
+//   var $h2TextContent = document.createTextNode(entries.title);
+//   $divColHalfTwo.appendChild($h2);
+//   $h2.appendChild($h2TextContent);
+
+//   var $paragraph = document.createElement('p');
+//   var $paragraphText = document.createTextNode(entries.notes);
+//   $divColHalfTwo.appendChild($paragraph);
+//   $paragraph.appendChild($paragraphText);
 // }
