779f0823afcade7fa84bbe7f72fe22101e5dc905
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-31 22:52:01 -0700

    Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 var $img = document.querySelector('img');
 var $inputUrl = document.querySelector('input[type="url"]');
 var $journalForm = document.querySelector('#journal-form');
-// var $ul = document.querySelector('#entries-list');
+var $ul = document.querySelector('#entries-list');
 
 function updatePhoto(event) {
   $img.src = $journalForm.elements.url.value;
@@ -24,35 +24,45 @@ function submission(event) {
 $inputUrl.addEventListener('input', updatePhoto);
 $journalForm.addEventListener('submit', submission);
 
-// function renderEntries(entries) {
-//   var $li = document.createElement('li');
-//   $ul.appendChild($li);
+function renderEntries(entries) {
+  var $li = document.createElement('li');
 
-//   var $divRow = document.createElement('div');
-//   $divRow.setAttribute('class', 'row');
-//   $li.appendChild($divRow);
+  var $divRow = document.createElement('div');
+  $divRow.setAttribute('class', 'row');
+  $li.appendChild($divRow);
 
-//   var $divColHalfOne = document.createElement('div');
-//   $divColHalfOne.setAttribute('class', 'column-half');
-//   $divRow.appendChild($divColHalfOne);
+  var $divColHalfOne = document.createElement('div');
+  $divColHalfOne.setAttribute('class', 'column-half');
+  $divRow.appendChild($divColHalfOne);
 
-//   var $img = document.createElement('img');
-//   $img.setAttribute('src', entries.photo);
-//   $img.setAttribute('alt', 'journal-image');
-//   $img.setAttribute('class', 'input-box-width border-radius');
-//   $divColHalfOne.appendChild($img);
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entries.photo);
+  $img.setAttribute('alt', 'journal-image');
+  $img.setAttribute('class', 'input-box-width border-radius');
+  $divColHalfOne.appendChild($img);
 
-//   var $divColHalfTwo = document.createElement('div');
-//   $divColHalfTwo.setAttribute('class', 'column-half');
-//   $divRow.appendChild($divColHalfTwo);
+  var $divColHalfTwo = document.createElement('div');
+  $divColHalfTwo.setAttribute('class', 'column-half');
+  $divRow.appendChild($divColHalfTwo);
 
-//   var $h2 = document.createElement('h2');
-//   var $h2TextContent = document.createTextNode(entries.title);
-//   $divColHalfTwo.appendChild($h2);
-//   $h2.appendChild($h2TextContent);
+  var $h2 = document.createElement('h2');
+  var $h2TextContent = document.createTextNode(entries.title);
+  $divColHalfTwo.appendChild($h2);
+  $h2.appendChild($h2TextContent);
 
-//   var $paragraph = document.createElement('p');
-//   var $paragraphText = document.createTextNode(entries.notes);
-//   $divColHalfTwo.appendChild($paragraph);
-//   $paragraph.appendChild($paragraphText);
-// }
+  var $paragraph = document.createElement('p');
+  var $paragraphText = document.createTextNode(entries.notes);
+  $divColHalfTwo.appendChild($paragraph);
+  $paragraph.appendChild($paragraphText);
+
+  return $li;
+}
+
+function entryTreeCreation(event) {
+  for (var entry = 0; entry < data.entries.length; entry++) {
+    renderEntries(data.entries[entry]);
+    $ul.appendChild(renderEntries(data.entries[entry]));
+  }
+}
+
+window.addEventListener('DOMContentLoaded', entryTreeCreation);
