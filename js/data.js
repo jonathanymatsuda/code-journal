c9ecaea18e334b08dab206e852accdc8b94ca741
Joe King <joe.king@email.org>
2021-11-05 10:09:15 -0700

    Last step in progress

--- js/data.js
+++ js/data.js
@@ -7,14 +7,27 @@ var data = {
   nextEntryId: 1
 };
 var previousSubmits = localStorage.getItem('entries-data');
+var $applicationView = document.querySelectorAll('.application-view');
 
 function storeData(event) {
   var previousSubmitsJSON = JSON.stringify(data);
   localStorage.setItem('entries-data', previousSubmitsJSON);
 }
 
+function switchViews(viewName) {
+  for (var view = 0; view < $applicationView.length; view++) {
+    if ($applicationView[view].getAttribute('data-view') === viewName) {
+      $applicationView[view].className = 'application-view';
+    } else {
+      $applicationView[view].className = 'hidden application-view';
+    }
+  }
+  data.view = viewName;
+}
+
 if (previousSubmits !== null) {
   data = JSON.parse(previousSubmits);
+  switchViews(data.entries);
 }
 
 window.addEventListener('beforeunload', storeData);
